67248fa664970c29530480e13eb55ced9341cf4a
UserHab <vitalibembel1.21@gmail.com>
2022-04-11 20:47:22 +0300

    commit_2

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Calculator {
     public static void main(String[] args) throws IOException {
-        System.out.println("Calculator");
+        System.out.println("Calculator_1");
         Assembly.assemblyFunction();
     }
 }
